c7f01d22c2afb8ba5e92f04e604a4791b3965d43
davidj288 <davidj28827@gmail.com>
2016-03-19 11:10:48 +1100

    Class pages almost completed

--- template-about.php
+++ template-about.php
@@ -8,25 +8,96 @@
 ?>
 
 <?php get_header(); ?>
-			<div id="content cf" class="page_content">
+<div id="content cf" class="page_content">
 
-				<div id="inner-content" class="wrap cf">
+	<div id="inner-content" class="wrap cf">
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-						<header class="article-header">
-							<h1 class="page-title"><?php the_title(); ?></h1>
-						</header>
+			<header class="article-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+			</header>
 
-						<section class="entry-content cf" itemprop="articleBody">
-							<div class="content">
+			<section class="entry-content cf" itemprop="articleBody">
+				<div class="content">
+					<?php 
+						the_content();
+					?>
+				</div>
+				<div class="about_content">
+					<?php
+						$top_h = get_field('top_section_heading');  
+						$top_content = get_field('top_content');
+
+						if($top_h && $top_content)
+						{
+					?>
+							<div class="top">
+								<h2 class="heading">
+									<?php echo $top_h; ?>
+								</h2>
+								<div class="contentwrap">
+									<?php echo $top_content; ?>
+								</div>
+							</div>							
+					<?php  
+						}
+					?>
+
+					<?php  
+						if( have_rows('teachers') ) : ?>
+							<div class="teachers">
+								<h2 class="heading">Teachers</h2>
 								<?php 
-									the_content();
+									while( have_rows('teachers') ) : the_row(); 
+										$name = get_sub_field('name');
+										$image = get_sub_field('image');
+										$description = get_sub_field('description');
+
+										if($name && $image && $description)
+										{
+								?>
+											<div class="teacher clearfix">
+												<div class="image">
+													<?php echo acf_image($image, 'large') ?>
+												</div>
+												<div class="text">
+													<h3 class="name">
+														<?php echo $name; ?>
+													</h3>
+													<p class="description">
+														<?php echo $description; ?>
+													</p>
+												</div>
+											</div>
+								<?php
+										} 
+									endwhile; 
 								?>
 							</div>
-							
-						</section>
-					</article>
+					<?php 
+						endif; //End Teachers
+					
+						$bottom_h = get_field('bottom_section_heading');  
+						$bottom_content = get_field('bottom_content');
+
+						if($bottom_h && $bottom_content)
+						{
+					?>
+							<div class="bottom">
+								<h2 class="heading">
+									<?php echo $bottom_h; ?>
+								</h2>
+								<div class="contentwrap">
+									<?php echo $bottom_content; ?>
+								</div>
+							</div>							
+					<?php  
+						}
+					?>
 				</div>
-			</div>
+			</section>
+		</article>
+	</div>
+</div>
 <?php get_footer(); ?>
--- template-singleclass.php
+++ template-singleclass.php
@@ -8,25 +8,98 @@
 ?>
 
 <?php get_header(); ?>
-			<div id="content cf" class="page_content">
+<?php the_post(); ?>
+<div id="content cf" class="page_content">
 
-				<div id="inner-content" class="wrap cf">
+	<div id="inner-content" class="wrap cf">
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-						<header class="article-header">
-							<h1 class="page-title"><?php the_title(); ?></h1>
-						</header>
+			<header class="article-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+				<?php  
+					$subtitle = get_field('subtitle');
 
-						<section class="entry-content cf" itemprop="articleBody">
-							<div class="content">
-								<?php 
-									the_content();
-								?>
-							</div>
-							
-						</section>
-					</article>
+					if($subtitle)
+					{
+				?>
+						<h2 class="subtitle"><?php echo $subtitle; ?></h2>
+				<?php  
+					}
+				?>
+			</header>
+
+			<section class="entry-content cf" itemprop="articleBody">
+				<div class="content">
+					<?php 
+						the_content();
+					?>
 				</div>
-			</div>
+				<?php 
+					if( have_rows('class') ) : 
+				?>
+						<div class="classes">
+							<?php 
+								while( have_rows('class') ) : the_row();
+									$image = get_sub_field('image');
+									$title = get_sub_field('title');
+									$subt = get_sub_field('subtitle');
+									$short = get_sub_field('short_content');
+									$more_info = get_sub_field('more_info_content');
+
+							?>
+								<div class="the_class">
+									<div class="image">
+										<?php echo acf_image($image, 'large'); ?>
+									</div>
+									<div class="contentwrap">
+										<h4 class="class_name">
+												<?php echo $title; ?>
+											<span class="subtitle">
+												<?php echo " - " . $subt; ?>
+											</span>
+										</h4>
+										<?php 
+											if( have_rows('daysandtimes') ) : 
+										?>
+												<ul class="daysandtimes">
+													<?php while( have_rows('daysandtimes') ) : the_row(); ?>
+															<li><?php the_sub_field('daytime'); ?></li>
+													<?php endwhile; ?>
+												</ul>
+										<?php 
+											endif; 
+
+											if($short)
+											{
+										?>	
+												<div class="short_content">
+													<?php echo $short; ?>
+												</div>
+										<?php
+											}
+
+											if($more_info)
+											{
+										?>
+												<button class="more_info">More Info</button>
+												<div class="morecontent">
+													<?php echo $more_info ?>
+												</div>
+										<?php  
+											}
+										?>
+									</div>
+								</div>
+							<?php 
+								endwhile; 
+							?>
+						</div>
+				<?php 
+					endif; 
+				?>
+			</section>
+		</article>
+	</div>
+</div>
 <?php get_footer(); ?>
